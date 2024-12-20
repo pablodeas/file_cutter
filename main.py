9b4e449bff9ffc52214e004566cb763cacf55ff4
pablodeas <pablodeas@gmail.com>
2024-12-20 15:38:15 -0300

    version 0.0.1

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ import shutil
 import glob
 import pandas as pd
 
-"""
-shutil.copy2(f'{ab}', f'{a}_ASE.xls')
-shutil.copy2(f'{ab}', f'{a}_IQ.xls')
-shutil.copy2(f'{ab}', f'{a}_SQL.xls')
-shutil.copy2(f'{ab}', f'{a}_ORACLE.xls')
-print("> Arquivos criados")
-print('>')
-"""
-
 a = input("> Digite o nome da planilha: ")
 
 for f in glob.glob(f'{a}.*'):
@@ -23,9 +14,8 @@ def strip_values(series):
 
 def create_new_sheet(bd_type, extention):
     try:
-
         df['tecnologia'] = strip_values(df['tecnologia'])
-
+        
         if df['tecnologia'].eq(bd_type).any():
             df_filtered = df[df['tecnologia'] == bd_type]    
             df_filtered.to_excel(f'{a}_{bd_type}{extention}', index=False)
@@ -39,24 +29,32 @@ def main():
     try:
         match ab:
             case s if s.endswith('.ods'):
-                uniq_tech = set(df['tecnologia'])
-                for t in uniq_tech:
-                    create_new_sheet(t, '.ods')
+                create_new_sheet('ASE', '.ods')
+                create_new_sheet('IQ', '.ods')
+                create_new_sheet('SQL', '.ods')
+                create_new_sheet('ORACLE', '.ods')
+                create_new_sheet('GCP-MYSQL', '.ods')
 
             case s if s.endswith('.xlsx'):
-                uniq_tech = set(df['tecnologia'])
-                for t in uniq_tech:
-                    create_new_sheet(t, '.xlsx')
+                create_new_sheet('ASE', '.xlsx')
+                create_new_sheet('IQ', '.xlsx')
+                create_new_sheet('SQL', '.xlsx')
+                create_new_sheet('ORACLE', '.xlsx')
+                create_new_sheet('GCP-MYSQL', '.xlsx')
 
             case s if s.endswith('.csv'):
-                uniq_tech = set(df['tecnologia'])
-                for t in uniq_tech:
-                    create_new_sheet(t, '.csv')
+                create_new_sheet('ASE', '.csv')
+                create_new_sheet('IQ', '.csv')
+                create_new_sheet('SQL', '.csv')
+                create_new_sheet('ORACLE', '.csv')
+                create_new_sheet('GCP-MYSQL', '.csv')
 
             case s if s.endswith('.xls'):
-                uniq_tech = set(df['tecnologia'])
-                for t in uniq_tech:
-                    create_new_sheet(t, '.xls')
+                create_new_sheet('ASE', 'xls')
+                create_new_sheet('IQ', 'xls')
+                create_new_sheet('SQL', 'xls')
+                create_new_sheet('ORACLE', 'xls')
+                create_new_sheet('GCP-MYSQL', 'xls')
 
             case _:
                 print('> Nenhum arquivo criado.')
